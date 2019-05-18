2e3ac8abd5c1695e84440f849b220442252af41b
Kevin Kössl <44550087+kevinkoessl@users.noreply.github.com>
2019-05-18 23:23:01 +0200

    Another Adjustment

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
 function delayedAlert() {
     var timeoutID = window.setTimeout(expandLogo, 800);
-    $('body').addClass('fadeInHalf');
+
 }
 function expandLogo() {
     $('.Name').addClass('is-expanded');
     var timeoutID = window.setTimeout(fadeInBanner, 2000);
+    window.setTimeout($('body').addClass('fadeInHalf'), 2000);
 }
 
 function fadeInBanner() {
