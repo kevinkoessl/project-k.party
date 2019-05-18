4875587748d44845e69c69d1d297a7cfa0e90537
Kevin Kössl <44550087+kevinkoessl@users.noreply.github.com>
2019-05-18 22:39:31 +0200

    Start Up
    Basic Layout for Project K Website.

---
+++ js/main.js
@@ -0,0 +1,14 @@
+
+function delayedAlert() {
+    var timeoutID = window.setTimeout(expandLogo, 800);
+    $('body').addClass('fadeInHalf');
+}
+function expandLogo() {
+    $('.Name').addClass('is-expanded');
+    var timeoutID = window.setTimeout(fadeInBanner, 2000);
+}
+
+function fadeInBanner() {
+    $('.main-banner').addClass('fadeIn');
+}
+delayedAlert();
