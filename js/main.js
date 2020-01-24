e12fa735461250b7393029b1a3672a7fdb0acaa0
Kevin Kössl <44550087+kevinkoessl@users.noreply.github.com>
2020-01-24 20:00:23 +0100

    Updated some stuff lol

--- js/main.js
+++ js/main.js
@@ -48,48 +48,63 @@ Vue.component('text-color-game', {
             currentStyle: {},
             gameMessage: "Drück den Knopf, wenn der Text Blau ist!",
             hitCounter: 0,
-            clicked: false
+            clicked: false,
+            wasBlue: false,
+            background: '#190d0b',
+            interval: null
         }
     }, computed: {},
     methods: {
         rotateColours() {
             let vm = this;
-            setInterval(function () {
-                let index;
-                for (let i = 0; i < 3; i++) {
-                    index = Math.floor(Math.random() * vm.colours.length);
-                    if (index !== 0) {
-                        break;
+            vm.interval = setInterval(function () {
+                if (!vm.wasBlue) {
+
+                    let index;
+                    for (let i = 0; i < 3; i++) {
+                        index = Math.floor(Math.random() * vm.colours.length);
+                        if (index !== 0) {
+                            break;
+                        }
                     }
-                }
-                if (vm.colourName === vm.colours[index].name) {
-                    vm.colourName = vm.colours[(index + 1) % vm.colours.length].name;
+                    if (vm.colourName === vm.colours[index].name) {
+                        vm.colourName = vm.colours[(index + 1) % vm.colours.length].name;
+                    } else {
+                        vm.colourName = vm.colours[index].name;
+                    }
+
+                    let index1 = Math.floor(Math.random() * vm.colours.length);
+                    let index2 = index1;
+                    do {
+                        index1 = Math.floor(Math.random() * vm.colours.length);
+                    } while (index2 === index1)
+                    setTimeout(function () {
+                        vm.currentColor = index2;
+                        if (index2 === 0) {
+                            vm.wasBlue = true;
+                        }
+                        vm.clicked = false;
+                    }, 70);
+
+                    vm.currentStyle = {
+                        backgroundColor: vm.colours[index1].colour,
+                        shadowColor: vm.colours[index2].colour,
+                        color: vm.colours[index2].colour
+                    }
                 } else {
-                    vm.colourName = vm.colours[index].name;
+                    vm.gameMessage = "Daneben. Trink einen Schluck!";
+                    vm.background = vm.colours[1].colour;
+                    vm.hitCounter = -1;s
+                    //clearInterval(vm.interval);
                 }
-
-                let index1 = Math.floor(Math.random() * vm.colours.length);
-                let index2 = index1;
-                do {
-                    index1 = Math.floor(Math.random() * vm.colours.length);
-                } while (index2 === index1)
-                setTimeout(function () {
-                    vm.currentColor = index2;
-                    vm.clicked = false;
-                }, 70);
-
-                vm.currentStyle = {
-                    backgroundColor: vm.colours[index1].colour,
-                    shadowColor: vm.colours[index2].colour,
-                    color: vm.colours[index2].colour
-                }
-            }, 700);
+            }, 500);
         },
         evaluateHit() {
             if (!this.clicked) {
                 this.clicked = true;
                 if (this.currentColor === 0) {
                     this.hitCounter++;
+                    this.wasBlue = false;
                     switch (this.hitCounter) {
                         case 5:
                             this.gameMessage = this.hitCounter + " Treffer! Drei Schlücke verteilen oder weitermachen.";
@@ -140,6 +155,12 @@ var app = new Vue({
     el: '#app',
     data: {
         message: 'Hello Vue!',
-        gameStarted: false
+        gameStarted: false,
+        backgroundColor: '#07070d'
+    }, methods: {
+        gameStopped(){
+            console.log('blub');
+            this.backgroundColor = '#b52732'
+        }
     }
 });
